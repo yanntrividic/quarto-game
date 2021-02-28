7fc80755e56d7ddbf864771a1bc597f32c0d9521
gyoza <78615246+elieduboux@users.noreply.github.com>
2021-02-28 02:23:23 +0100

    constantes added for differentes players

--- src/quarto/constants.py
+++ src/quarto/constants.py
@@ -17,6 +17,14 @@ SCOLS, SROWS = 8, 2  # storage board dimensions
 SXOFFSET = WIDTH // 2 - SQUARE_SIZE * SCOLS // 2
 SYOFFSET = HEIGHT - SQUARE_SIZE * (SROWS + 1)
 
+# P1COLS, P1ROWS = 2, 1 # player1 rectangle dimensions
+# P1XOFFSET = WIDTH - SQUARE_SIZE * (P1COLS + 1)
+# P1YOFFSET = SQUARE_SIZE * (P1ROWS + 2)
+
+# P2COLS, P2ROWS = 2, 1 # player2 rectangle dimensions
+# P2XOFFSET = P1XOFFSET 
+# P2YOFFSET = P1YOFFSET + SQUARE_SIZE
+
 # Colors
 GREEN = pg.Color('PaleGreen4')  # light green
 LGREEN = pg.Color('dark sea green')  # very light green
@@ -28,9 +36,14 @@ LBROWN = pg.Color('darkgoldenrod') #light brown
 BROWN = pg.Color('saddlebrown') #brown
 DBROWN = pg.Color((41, 21, 10)) #dark brown
 
+WHEAT = pg.Color('wheat')
+
 BG = (73, 67, 54) #BackGround
 FONT = "freesansbold.ttf" #the font used for the text in the game
 
 PLAYER1 = "PLAYER1" #the two players
 PLAYER2 = "PLAYER2"
+AI1 = "AI1" #the 3 artificial intelligence the player can play against
+AI2 = "AI2"
+AI3 = "AI3"
 TIE = "TIE" #if there is no winner
